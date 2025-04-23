0a1b50632031ec20e45c4810b6aa15922f763336
fabiopacifici <boolean@fabiopacifici.com>
2025-04-23 10:35:43 +0200

    feat: implement book creation functionality in CreateBook component

--- src/pages/admin/CreateBook.jsx
+++ src/pages/admin/CreateBook.jsx
@@ -8,11 +8,38 @@ const CreateBook = () => {
     cover_image: ''
   });
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const form = new FormData()
+    form.append('title', formData.title)
+    form.append('abstract', formData.abstract)
+    form.append('author', formData.author)
+    form.append('cover_image', formData.cover_image)
+
+
+    fetch('http://localhost:3000/api/v1/books/create', {
+      method: 'POST',
+      body: form,
+      credentials: 'include',
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data, 'Book created successfully');
+      })
+      .catch(err => {
+        console.error(err, 'Error creating book');
+      })
+  }
+
+
+
+
   return (
     <div className="container py-4">
       <h1 className="mb-4">Create New Book</h1>
 
-      <form className="col-md-6">
+      <form className="col-md-6" encType='multipart/form-data' onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="title" className="form-label">
             Title
@@ -22,6 +49,7 @@ const CreateBook = () => {
             id="title"
             name="title"
             value={formData.title}
+            onChange={(e) => setFormData({ ...formData, title: e.target.value })}
             className="form-control"
           />
         </div>
@@ -35,6 +63,7 @@ const CreateBook = () => {
             id="author"
             name="author"
             value={formData.author}
+            onChange={(e) => setFormData({ ...formData, author: e.target.value })}
             className="form-control"
           />
         </div>
@@ -47,6 +76,7 @@ const CreateBook = () => {
             id="abstract"
             name="abstract"
             value={formData.abstract}
+            onChange={(e) => setFormData({ ...formData, abstract: e.target.value })}
             rows="4"
             className="form-control"
           ></textarea>
@@ -57,10 +87,10 @@ const CreateBook = () => {
             Cover Image URL
           </label>
           <input
-            type="text"
+            type="file"
             id="cover_image"
             name="cover_image"
-            value={formData.cover_image}
+            onChange={(e) => setFormData({ ...formData, cover_image: e.target.files[0] })}
             className="form-control"
           />
         </div>
